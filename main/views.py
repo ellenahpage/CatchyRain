2a7844e1d59c250d29ca9113d54e4ae05277de5c
ellenahpage <36309778+ellenahpage@users.noreply.github.com>
2018-03-05 17:53:08 +0000

    Delete views.py

--- main/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def base(request):
-    return render(request, 'main/base.html')
-
-def whyRWH(request):
-    return HttpResponse("RWH Information")
